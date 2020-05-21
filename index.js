16f7ef21d30e5e24b32b1d769601b1a51f732b6b
Dragon Chow <dragonpxchow@gmail.com>
2020-05-21 23:39:51 +1000

    update Cors

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 //  added by Pei app.use
 // Enable CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://intense-lowlands-44905.herokuapp.com/api/Genres"
+  );
   res.header(
     "Access-Control-Allow-Methods",
     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
