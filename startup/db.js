2cb2a246d5baabde1551ad309c0d543acd37e9a9
Dragon Chow <dragonpxchow@gmail.com>
2020-05-29 00:50:07 +1000

    using MongoClient connect"

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,19 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+module.exports = function () {
+  //const db = config.get("db");
+  // mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+
+  const MongoClient = require("mongodb").MongoClient;
+  const uri = config.get("db");
+
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((err) => {
+    winston.info(`Connected to ${db}...   hello`);
+    //const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    //client.close();
+  });
+};
