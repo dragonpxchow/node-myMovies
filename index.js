e3535f09a35389e82002e7160b75df4db45b21ab
Dragon Chow <dragonpxchow@gmail.com>
2020-05-29 00:18:18 +1000

    remove cors

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
+/*
 //  added by Pei app.use
 // Enable CORS
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  /*
   res.header(
     "Access-Control-Allow-Methods",
     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
   );
-  */
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept" // Authorization"
   );
   next();
 });
+*/
 
 require("./startup/logging")();
 require("./startup/cors")(app);
