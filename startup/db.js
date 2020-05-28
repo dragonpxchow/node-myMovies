d5b2fbe8b074f23c3b0e0ea543094ba269e8f9db
Dragon Chow <dragonpxchow@gmail.com>
2020-05-29 01:02:11 +1000

    use back mongoose.connect"

--- startup/db.js
+++ startup/db.js
@@ -4,16 +4,16 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
-  // mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 
+  /*
   const MongoClient = require("mongodb").MongoClient;
   const uri = db;
-
   const client = new MongoClient(uri, { useNewUrlParser: true });
   client.connect((err) => {
-    winston.info(`Connected to ${db}...   hello`);
-    //const collection = client.db("test").collection("devices");
+    const collection = client.db("test").collection("devices");
     // perform actions on the collection object
-    //client.close();
+  /client.close();
   });
+  */
 };
